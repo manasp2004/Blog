b068d7b803dbb230689aea4b7181ee77f89451b8
Manas Prakash <manasp2004@gmail.com>
2021-01-27 21:26:47 +0530

    Form Created

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,28 @@
+import { useState } from "react"
+
 const Create = () => {
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+    const [author, setAuthor] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const blog = { title, body, author }
+        // setTitle("")
+        // setBody("")
+        // setAuthor("")
+    }
     return (
         <div className="create">
-            <button>Add a new blog</button>
+            <h1>Add a new blog</h1>
+            <form onSubmit={ handleSubmit }>
+                <label>Blog Title</label>
+                <input type="text" value={ title } onChange={(e) => setTitle(e.target.value)} spellCheck="false" required />
+                <label>Body</label>
+                <textarea value={ body } onChange={(e) => setBody(e.target.value)} required />
+                <label>Author</label>
+                <input type="text" value={ author } onChange={(e) => setAuthor(e.target.value)} spellCheck="false" required />
+                <button>Add Blog</button>
+            </form>
         </div>
     )
 }
